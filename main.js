8955f831404360fc28e650dadd35faa2d421a4bd
earoglif <earoglif@gmail.com>
2019-01-08 00:19:57 +0300

    Доработка запроса. Добавлены стили таблицы.

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
-$( document ).ready(function() {
+$( document ).ready( function() {
 
-    $("#searchBtn").click(function(){
-        $.get("./items.json", function(data, status){
-            console.log('ITEMS:', data, status);
-        });
-
+    $( "#searchBtn" ).click( function(){
+            $( ".items-block" ).removeClass( "display-block" );
+            $( ".empty-block" ).addClass( "display-none" );
+            $( ".load-block" ).removeClass( "display-none" );
         
+        $.get("./items.json", function( data, status ){
+            console.log('ITEMS:', data, status, );
+            var items = data || [];
+
+            if(status == 'success' && items.length) {
+                $( ".items-block" ).addClass( "display-block" );
+                $( ".empty-block" ).addClass( "display-none" );
+                $( ".load-block" ).addClass( "display-none" );
+                var content = '';
+
+                for (var i = 0; i < items.length; i++) {
+                    content += '<tr>';
+                    content += '<td>' + items[i].Name + '</td>';
+                    content += '<td>' + items[i].Name_channel + '</td>';
+                    content += '<td>' + items[i].time_end + '</td>';
+                    content += '<td>' + items[i].time_start + '</td>';
+                    content += '</tr>';
+                }
+
+                $('.items-block table tbody').html(content);
+            }
+        });
     });
 
 });
